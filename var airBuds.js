45e66f419776b9d74b9c4f7495bba04af570f342
sriram28528 <88391172+sriram28528@users.noreply.github.com>
2022-01-16 18:38:32 +0530

    Add files via upload

---
+++ var airBuds.js
@@ -0,0 +1,20 @@
+var airBuds
+var  Buds
+var  Buds
+var sound
+function setup(){
+createCanvas(800,800)
+
+airBuds=createSprite(200,300,200,400)
+airBuds.shapeColor="cyan"
+ Buds=createSprite(600,300,200,300)
+ Buds.shapeColor="black"
+ Buds=createSprite(400,400,500,400)
+ Buds.shapeColor="purple"
+
+
+function draw(){
+background("white")
+
+drawSprites()
+}
